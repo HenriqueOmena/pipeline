6459fc9ac524f95c3453fb969a53940bfefa8c8c
Henrique Omena <88lmao@gmail.com>
2019-11-06 17:34:20 +0000

    !WP done drag itens and i will work with context next time

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,11 +2,16 @@ import React, { useRef } from 'react';
 import './card.style.scss';
 import { useDrop, useDrag } from 'react-dnd';
 
-const Card = ({data}) => {
+const Card = ({data, index}) => {
     const ref = useRef()
 
     const [{ isDragging }, dragRef] = useDrag({
-        item: { type: 'CARD', id: data.id},
+        item: {
+            type: 'CARD',
+            index,
+            content: data.content,
+            id: data.id
+        },
         collect: monitor => ({
             isDragging: monitor.isDragging(),
         }),
@@ -15,8 +20,29 @@ const Card = ({data}) => {
     const [, dropRef] = useDrop({
         accept: 'CARD',
         hover(item, monitor) {
-            console.log(item.id);
-            console.log(data.id);
+            const draggedIndex = item.index;
+            const targetIndex = index;
+
+            if (draggedIndex === targetIndex) {
+                return
+            }
+
+            const targetSize = ref.current.getBoundingClientRect();
+            const targetCenter = (targetSize.bottom - targetSize.top) / 2;
+
+            const draggedOffset = monitor.getClientOffset();
+            const draggedTop = draggedOffset.y - targetSize.top;
+
+            if (draggedIndex - targetIndex && draggedTop < targetCenter) {
+                return
+            }
+
+            if (draggedIndex > targetIndex && draggedTop > targetCenter) {
+                return
+            }
+
+
+            console.log('object')
         }
     })
 
@@ -47,7 +73,7 @@ const Card = ({data}) => {
                 <p> { data.content }</p>
                 { data.user
                     ? <img src={data.user} alt="" />
-                    : <img src="https://api.adorable.io/avatars/115/abott@adorable.io.png" alt=""             />
+                    : <img src="https://api.adorable.io/avatars/115/abott@adorable.io.png" alt=""/>
                 }
             </div>
         </div>
--- src/components/list/list.component.jsx
+++ src/components/list/list.component.jsx
@@ -20,7 +20,7 @@ const List = ({data}) => {
 
             </div>
             <ul>
-                { data.cards.map( card => <Card key={card.id} data={card} />)}
+                { data.cards.map( (card, index) => <Card key={card.id} index={index} data={card} />)}
             </ul>
         </div>
     )
